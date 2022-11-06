deb49058cdf2d3f029dc42cceed27c12d792e80e
Neodevils <neoaichan@gmail.com>
2022-11-06 17:00:13 +0300

    Fast package making for now.

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {string} text - The text to check for the word "Neo"
+ * @returns {boolean} Returns true if the string equals to neo
+ */
+
+export function isNeo(text) {
+	return text === "Neo";
+}
